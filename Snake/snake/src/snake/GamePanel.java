5d0563376268d3867364c0991fdb5f96d076e58c
Eric-windows <eric.babcock@protonmail.com>
2023-01-06 11:27:21 +0100

    added variable to snake

--- Snake/snake/src/snake/GamePanel.java
+++ Snake/snake/src/snake/GamePanel.java
@@ -11,8 +11,22 @@ public class GamePanel extends JPanel implements ActionListener{
 	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
 	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesEaten;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+			
+			
+			
 	GamePanel(){
-	
+		random = new Random();
+		this.setPreferredSize(new Dimension());
 	}
 	public void startGame() {
 		
